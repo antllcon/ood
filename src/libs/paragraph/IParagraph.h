2d97810619e6a0fa451aa2024ecd6d63832f622d
antllcon <pokemonivan32@gmail.com>
2025-10-30 15:53:09 +0300

    Запретил оператор и конструктор присваивания

--- src/libs/paragraph/IParagraph.h
+++ src/libs/paragraph/IParagraph.h
@@ -6,6 +6,12 @@ class IParagraph
 public:
 	virtual ~IParagraph() = default;
 
+	IParagraph(const IParagraph&) = delete;
+	IParagraph& operator=(const IParagraph&) = delete;
+
 	virtual const std::string& GetText() const = 0;
 	virtual void SetText(const std::string& text) = 0;
+
+protected:
+	IParagraph() = default;
 };
